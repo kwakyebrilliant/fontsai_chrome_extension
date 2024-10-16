144afef711e5328a1b9ad21e60ba52177cc4071d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-10-16 19:02:19 +0300

    main page updates

--- lib/Auth/main_page.dart
+++ lib/Auth/main_page.dart
@@ -12,12 +12,20 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(26, 26, 26, 1),
+        elevation: 0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("FontAI"),
+            Text(
+              "FontAI",
+              style: TextStyle(
+                color: const Color.fromRGBO(255, 255, 255, 1),
+              ),
+            ),
             Icon(
               Icons.toggle_on_outlined,
+              color: const Color.fromRGBO(255, 255, 255, 1),
             ),
           ],
         ),
