5db824589589e2eb9b672c7f8899df56acfc43bc
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-10-18 21:38:23 +0300

    screens added

---
+++ lib/Screens/information_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InformationPage extends StatefulWidget {
+  const InformationPage({super.key});
+
+  @override
+  State<InformationPage> createState() => _InformationPageState();
+}
+
+class _InformationPageState extends State<InformationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
